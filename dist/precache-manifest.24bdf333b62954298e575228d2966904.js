f2db2c72b63be2386f7e511df8513df94d3c1330
zhanglei <373370750@qq.com>
2019-09-02 15:45:03 +0800

    完善图片剪裁

---
+++ dist/precache-manifest.24bdf333b62954298e575228d2966904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25556044be8773c54cd38f6b2b9fcb21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b48b6a5bdcdf87baa43",
+    "url": "./static/css/2.e4a1781d.chunk.css"
+  },
+  {
+    "revision": "c0f943eb68ef8e8ba8f3",
+    "url": "./static/css/main.3227a235.chunk.css"
+  },
+  {
+    "revision": "6b48b6a5bdcdf87baa43",
+    "url": "./static/js/2.fdb8b2f0.chunk.js"
+  },
+  {
+    "revision": "c0f943eb68ef8e8ba8f3",
+    "url": "./static/js/main.3a725a2c.chunk.js"
+  },
+  {
+    "revision": "cc889a8dcecddd37cc0a",
+    "url": "./static/js/runtime~main.51fc77b0.js"
+  }
+]);
